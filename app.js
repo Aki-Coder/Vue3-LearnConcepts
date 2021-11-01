f3512f880396ad34d9a79d64234b2305c51c8b67
Andreja Simić <andrejas@yuspin.rs>
2021-11-01 13:45:31 +0100

    locking content with v-once

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = Vue.createApp({
             courseGoalA:'Finish the course and learn Vue',
             courseGolasB:'Master Vue and build amazing apps',
             vueLink:'https://vuejs.org/',
-            counter:0,
+            counter:5,
             name:'',
             confirmedName:'',
         }
